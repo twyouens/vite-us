ff8461ec8a242b273c2b8fba5870ace8a034d2c1
Thomas <t.w.youens2@newcastle.ac.uk>
2023-06-23 16:03:53 +0100

    switch dom to use router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import AppRouter from './router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppRouter/>
   </React.StrictMode>,
 )
